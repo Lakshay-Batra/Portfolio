1f48db7b5428f5467d39273c3ec44ba2940d3e25
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-04-27 02:00:04 +0530

    resolve bug

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $("#mode").on("click", () => {
     $("nav").toggleClass("navbar-light bg-light navbar-dark bg-secondary");
-    $("#mode-logo").toggleClass("far fa-moon fas fa-adjust");
+    $("#mode-logo").toggleClass("fa-moon fa-adjust");
     $("body").toggleClass("bg-dark text-light");
     $("hr").toggleClass("hr-white");
     $("h1").toggleClass("h1-light");
+    $(".center-logo").toggleClass("text-secondary")
     $("#footer").toggleClass("bg-dark bg-secondary")
 });
