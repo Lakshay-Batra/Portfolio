f96352272ba65946401d0b2b5a572c54a17ff00f
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-04-27 02:36:06 +0530

    Update design

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 $("#mode").on("click", () => {
     $("nav").toggleClass("navbar-light bg-light navbar-dark bg-secondary");
-    $("#mode-logo").toggleClass("fa-moon fa-adjust");
+    $("#mode-logo").toggleClass("fa-moon fa-adjust text-secondary");
     $("body").toggleClass("bg-dark text-light");
     $("hr").toggleClass("hr-white");
-    $("h1").toggleClass("h1-light");
-    $(".center-logo").toggleClass("text-secondary")
-    $("#footer").toggleClass("bg-dark bg-secondary")
+    $("h1").toggleClass("h1-light title-background");
+    $(".center-logo").toggleClass("text-secondary");
+    $("#footer").toggleClass("bg-dark bg-secondary");
+    // $("#home .col-md-4").toggleClass("title-background");
 });
